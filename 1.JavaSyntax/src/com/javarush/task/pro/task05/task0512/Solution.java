bd16686e03537f93a7dadacd40bfb25df0ce05c9
Nastya <nastemorozovoi@gmail.com>
2021-08-07 17:28:17 +0300

    initialized commit

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0512/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task05.task0512;
+
+/* 
+Создаем мультимассив
+*/
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static int[][][] multiArray = new int[][][]{{{4, 8, 15}, {16}}, {{23, 42}, {}}, {{1}, {2}, {3}, {4, 5}}};
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        for (int i = 0; i < multiArray.length; i++)
+        {
+            for (int j = 0; j < multiArray[i].length; j++)
+            {
+                for (int g = 0; g < multiArray[i][j].length; g++)
+                {
+                    System.out.println(Arrays.toString(multiArray[i][j]) + " ");
+                }
+            }
+            System.out.println(Arrays.deepToString(multiArray[i]) + " ");
+        }
+        System.out.println();
+    }
+}
