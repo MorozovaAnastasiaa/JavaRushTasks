574b028bdcbc5c23e22d109d9b3ed7fb42e8cd1c
Nastya <nastemorozovoi@gmail.com>
2021-09-10 18:23:49 +0300

    initialized commit

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,58 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(int a){
+        a = 1;
+    }
+
+    public static void universalMethod(String b){
+        b = "hi";
+    }
+    public static void universalMethod(String a, String b){
+        a = "i";
+        b = "am Nastya";
+    }
+    public static void universalMethod(String a, int c){
+        a = "i'm";
+        c = 16;
+    }
+
+    public static void universalMethod(int d, int b){
+        d = 3;
+        b = 2;
+    }
+    public static void universalMethod(int d, int b, int c, String a){
+        d = 1;
+        b = 2;
+        c = 3;
+    }
+    public static void universalMethod(int d, int b, int c, int a){
+        d = 1;
+        b = 2;
+        c = 3;
+    }
+    public static void universalMethod(int d, int b, int c, int a, int f){
+        d = 1;
+        b = 2;
+        c = 3;
+    }
+    public static void universalMethod(int d, int b, int c, String q, String k){
+        d = 1;
+        b = 2;
+        c = 3;
+    }
+    public static void universalMethod(int d, int b, int c, String f, String m, int i){
+        d = 1;
+        b = 2;
+        c = 3;
+    }
+}
