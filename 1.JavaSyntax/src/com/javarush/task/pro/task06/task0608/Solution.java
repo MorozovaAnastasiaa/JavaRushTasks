76d9fd81d8847666e06ff98c87cf8b33299650d2
Nastya <nastemorozovoi@gmail.com>
2021-09-11 21:13:59 +0300

    initialized commit

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0608/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task06.task0608;
+
+/* 
+Кубический калькулятор
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //напишите тут ваш код
+    public static long cube(long a) {
+
+        long result = a * a * a;
+        return result;
+    }
+}
