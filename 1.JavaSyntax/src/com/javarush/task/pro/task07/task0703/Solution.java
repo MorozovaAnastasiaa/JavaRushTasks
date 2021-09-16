74ebcd14c8e058eb244cd63fc963029272f10304
Nastya <nastemorozovoi@gmail.com>
2021-09-16 21:54:25 +0300

    Минимальное целое Создай по одной переменной каждого примитивного целочисленного типа в классе Solution. Этим переменным присвой минимально возможное для них значение. Никаких методов создавать не нужно.
    Requirements:
    1. В классе Solution должны быть созданы переменные всех примитивных целочисленного типов.
    2. Всем целочисленным переменным должно быть присвоено минимально возможное для них значение.
    3. В классе Solution не должно быть создано никаких методов.

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    //напишите тут ваш код
+    byte first = -128;
+    short second = -32768;
+    int third = -2147483648;
+    long fourth = -9223372036854775808L;
+}
