7c14ef5a1361cf86c62f391afb43de8dc8fe200d
Nastya <nastemorozovoi@gmail.com>
2021-09-16 21:30:11 +0300

    Все возможные типы Как тебе уже известно, всего в языке Java 8 примитивных типов. Создай по одной переменной каждого примитивного типа в классе Solution. Переменные не должны находиться в каком-либо методе.
    Requirements:
    1. В классе Solution должны быть созданы переменные всех примитивных типов.

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    //напишите тут ваш код
+    byte a = 1;
+    boolean b = a > 0;
+    int c = 1113;
+    double d = 1.6;
+    float e = 3;
+    short f = 0;
+    long g = 1773757;
+    char h = 1;
+}
