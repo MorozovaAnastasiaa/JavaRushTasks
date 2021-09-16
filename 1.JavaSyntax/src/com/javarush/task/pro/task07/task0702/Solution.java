d16263a02ac6f7369c21f732e35bdfdd4b376845
Nastya <nastemorozovoi@gmail.com>
2021-09-16 21:49:51 +0300

    Максимальное целое Создай по одной переменной каждого примитивного целочисленного типа в классе Solution. Этим переменным присвой максимально возможное для них значение. Никаких методов создавать не нужно.
    Requirements:
    1. В классе Solution должны быть созданы переменные всех примитивных целочисленного типов.
    2. Всем целочисленным переменным должно быть присвоено максимально возможное для них значение.
    3. В классе Solution не должно быть создано никаких методов.

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    //напишите тут ваш код
+    int a = 2147483647;
+    long b = 9223372036854775807L;
+    short c = 32767;
+    byte d = 127;
+}
