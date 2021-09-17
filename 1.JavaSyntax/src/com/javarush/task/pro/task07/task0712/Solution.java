d980527efa86745de73735845c2e441d361d2e5d
Nastya <nastemorozovoi@gmail.com>
2021-09-17 21:21:19 +0300

    Первые объекты Используя оператор new в методе main(), создай:
    10 объектов типа String;
    5 объектов типа int[] (массивов значений int);
    2 объекта типа Scanner.
    
    Requirements:
    1. В методе main() должно создаваться 10 объектов типа String.
    2. В методе main() должно создаваться 5 объектов типа int[].
    3. В методе main() должно создаваться 2 объекта типа Scanner.

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String note = new String("aaaaa");
+        String note1 = new String("aaaaa");
+        String note2 = new String("aaaaa");
+        String note3 = new String("aaaaa");
+        String note4 = new String("aaaaa");
+        String note5 = new String("aaaaa");
+        String note6 = new String("aaaaa");
+        String note7 = new String("aaaaa");
+        String note8 = new String("aaaaa");
+        String note9 = new String("aaaaa");
+
+        int[] number = new int[10];
+        int[] number1 = new int[10];
+        int[] number2 = new int[10];
+        int[] number3 = new int[10];
+        int[] number4 = new int[10];
+
+        Scanner write = new Scanner(System.in);
+        Scanner write1 = new Scanner(System.in);
+
+
+
+    }
+}
