8caf231c6161db4785789daccfc8afc1a275ed0b
Nastya <nastemorozovoi@gmail.com>
2021-09-23 22:11:16 +0300

    Минимальный элемент массива В этой задаче тебе нужно:
    Считать 10 чисел с консоли и заполнить ими массив с помощью метода getArrayOfTenElements().
    Найти минимальный элемент массива и вернуть этот элемент с помощью метода min(int[]).
    В методе min(int[]) обязательно используй метод Math.min(int, int).
    
    Requirements:
    1. Программа должна считывать числа с клавиатуры.
    2. Класс Solution должен содержать только три метода.
    3. Метод getArrayOfTenElements() должен считать с клавиатуры 10 чисел, потом вернуть массив размером 10 элементов, заполненный считанными числами.
    4. Метод min(int[]) должен вернуть минимальный элемент массива, используя метод min(int, int) класса Math.
    5. Метод main() должен вызывать метод getArrayOfTenElements().
    6. Метод main() должен вызывать метод min(int[]).

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0803/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.pro.task08.task0803;
+
+import java.util.Scanner;
+
+/* 
+Минимальный элемент массива
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] intArray = getArrayOfTenElements();
+        System.out.println(min(intArray));
+    }
+
+    public static int min(int[] intArray) {
+        //напишите тут ваш код
+        int a = Integer.MAX_VALUE;
+        for (int i = 0; i < intArray.length; i++)
+        {
+            a = Math.min(a, intArray[i]);
+        }
+        return a;
+    }
+
+    public static int[] getArrayOfTenElements() {
+        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+
+        int[] intArray = new int[10];
+        for (int i = 0; i < intArray.length; i++)
+            intArray[i] = scanner.nextInt();
+
+        return intArray;
+    }
+}
