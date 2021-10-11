9465da6f1aad619e1621c585e4d6f68c95e2b9e4
Nastya <nastemorozovoi@gmail.com>
2021-10-11 20:50:49 +0300

    Восьмеричный конвертер Публичный статический метод toOctal(int) должен переводить целое число, полученное в качестве входящего параметра, из десятичной системы счисления в восьмеричную. А публичный статический метод toDecimal(int) наоборот — из восьмеричной в десятичную. Методы работают только с положительными числами. Если входящий параметр меньше или равен 0, методы возвращают 0. Твоя задача — реализовать эти методы.
    Один из алгоритмов перевода десятичного числа в восьмеричное следующий:
    i равно 0
    while(десятичное число не равно 0) {
    восьмеричное число = восьмеричное число + (остаток от деления десятичного числа на 8) * на 10 в степени i
    десятичное число = десятичное число / 8
    i увеличиваем на 1
    }
    
    Один из алгоритмов перевода восьмеричного числа в десятичное следующий:
    i равно 0
    while(восьмеричное число не равно 0) {
    десятичное число = десятичное число + (остаток от деления восьмеричного числа на 10) * на 8 в степени i
    восьмеричное число = восьмеричное число / 10
    i увеличиваем на 1
    }
    
    Метод main() не принимает участие в тестировании.
    
    Подсказка: чтобы возвести число в степень, можно использовать метод Math.pow(число, степень).
    
    Requirements:
    1. Нужно чтобы метод toOctal(int) был реализован согласно условию.
    2. Нужно чтобы метод toDecimal(int) был реализован согласно условию.
    3. Методы Integer.toOctalString(int) и Long.toOctalString(int) использовать нельзя.
    4. Методы Integer.parseInt(String, int) и Long.parseLong(String, int) использовать нельзя.
    5. Методы Integer.toString(int, int) и Long.toString(long, int) использовать нельзя.
    6. Объект типа BigInteger использовать нельзя.
    7. Объект типа BigDecimal использовать нельзя.

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0905/Solution.java
@@ -0,0 +1,50 @@
+package com.javarush.task.pro.task09.task0905;
+
+import java.util.regex.Pattern;
+
+/* 
+Восьмеричный конвертер
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int decimalNumber = 21;
+        System.out.println("Десятичное число " + decimalNumber + " равно восьмеричному числу " + toOctal(decimalNumber));
+        int octalNumber = 25;
+        System.out.println("Восьмеричное число " + octalNumber + " равно десятичному числу " + toDecimal(octalNumber));
+    }
+
+    public static int toOctal(int decimalNumber) {
+        //напишите тут ваш код
+        int i = 0;
+        int g = decimalNumber;
+        double octalNumber = 0;
+        while (decimalNumber != 0)
+        {
+            octalNumber = octalNumber + (decimalNumber % 8) * Math.pow(10, i);
+            decimalNumber = decimalNumber / 8;
+            i++;
+        }
+        if (g > 0)
+            return (int)octalNumber;
+        else
+            return 0;
+    }
+
+    public static int toDecimal(int octalNumber) {
+        //напишите тут ваш код
+        int i = 0;
+        int a = octalNumber;
+        double decimalNumber = 0;
+        while (octalNumber != 0)
+        {
+            decimalNumber = decimalNumber + (octalNumber % 10) * Math.pow(8, i);
+            octalNumber = octalNumber / 10;
+            i++;
+        }
+        if (a > 0)
+            return (int)decimalNumber;
+        else
+            return 0;
+    }
+}
