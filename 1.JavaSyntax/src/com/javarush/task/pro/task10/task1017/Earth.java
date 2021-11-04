46d77d11458aca18a861bf9ad45fda06f437f17e
Nastya <nastemorozovoi@gmail.com>
2021-11-04 18:03:41 +0300

    Создание материков Как известно, на планете Земля есть 6 материков. Создай для каждого материка собственный класс. В каждом классе должно быть поле, которое хранит площадь соответствующего материка. Площадь материка нужно указывать при его создании, и она должна быть неизменяемой. Ты можешь использовать любые числа для задания площади материков.
    Requirements:
    1. В отдельных файлах создай публичные классы Africa, Antarctica, Australia, Eurasia, NorthAmerica, SouthAmerica.
    2. В каждом из созданных классов должно быть private final int поле area.
    3. В каждом из созданных классов должен быть публичный конструктор с аргументом типа int, который будет инициализировать поле area.
    4. В методе main класса Earth создай 6 разных материков и передай в них значения площадей.

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1017/Earth.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task10.task1017;
+
+/* 
+Создание материков
+*/
+
+public class Earth {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Africa africa = new Africa(5436);
+        Antarctica antarctica = new Antarctica(66666);
+        Australia australia = new Australia(3452);
+        Eurasia eurasia = new Eurasia(101010);
+        NorthAmerica northAmerica = new NorthAmerica(71717);
+        SouthAmerica southAmerica = new SouthAmerica(99999999);
+    }
+}
